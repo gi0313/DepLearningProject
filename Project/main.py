2354a2e16f79d160a530fb8847af5658825ba9b4
Seraj Noureddine <snoureddine01@outlook.com>
2026-04-14 23:25:04 -0400

    changes to main

--- Project/main.py
+++ Project/main.py
@@ -37,8 +37,8 @@ if __name__ == "__main__":
 
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.ToTensor()
-        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     dataset = datasets.ImageFolder("./data/space images", transform=transform)
